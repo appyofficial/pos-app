621524824981e784c932661b2af046412eaa38fc
appyofficial <appy70890@gmail.com>
2020-03-01 23:05:28 +0100

    update

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import './Checkout.css';
 
 export default class Checkout extends Component {
+    handleGoBack = () => {
+        this.props.goBack();
+    }
+
+    totalBill = () => {
+        let cost = this.props.checkoutItems.reduce((a, b) => a + b.price, 0);
+        return cost;
+    }
+
     render() {
         return (
-            <div className='checkout'>
-                <div >
+            <div className='checkout' style={{ display: this.props.cartActive ? 'block' : 'none' }}>
+                <div className='checkout-container'>
+                    <h2>Your Order</h2>
                     <div className='checkout-items'>
-
+                        {this.props.checkoutItems.map(item => (
+                            <div className='checkout-item-card'>
+                                <h3>{item.name}</h3>
+                                <p>{item.price} SEK</p>
+                            </div>
+                        ))}
+                        <div className='checkout-bill'><span>Total:</span> <span>{this.totalBill()} SEK</span></div>
                     </div>
-
                     <div className='checkout-btns-container'>
-                        <Link>Go Back</Link>
+                        <button onClick={this.handleGoBack}>Go Back</button>
                         <button>Pay</button>
                     </div>
                 </div>
--- src/Pages/StartPage/StartPage.js
+++ src/Pages/StartPage/StartPage.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import './StartPage.css';
-import orderHereImg from '../../Data/images/order-here.png';
+import eatHere from '../../Data/icons/eathere.png';
+import takeAway from '../../Data/icons/takeawa.png';
+import homeImage from '../../Data/images/home-image.jpg';
 
 export default class StartPage extends Component {
     render() {
         return (
             <div className="startpage">
                 <div className="startpage-container">
-                    <div className="startpage-img">
-                        <img src={orderHereImg} alt='Order here' />
-                    </div>
+                    <span className='madeBy'>Made by <a href='https://www.appysharma.com'>Appy Sharma</a></span>
                     <div className="startpage-content">
                         <h1>Order Food</h1>
+                        <p>Fresh food right outta kitchen.<br /> Choose from many different food and drinks.</p>
                         <div className='startpage-btns'>
-                            <Link className='startBtn' to='/home'>Eat here</Link>
-                            <Link className='startBtn' to='/home'>Take away</Link>
+                            <Link className='startBtn' to='/home'>
+                                <img src={eatHere} alt='' />Eat here</Link>
+                            <Link className='startBtn' to='/home'>
+                                <img src={takeAway} alt='' />Take away</Link>
                         </div>
                     </div>
+                </div>
+                <div className='startup-image' style={{ backgroundImage: `url(${homeImage})` }}>
                 </div>
             </div>
         );

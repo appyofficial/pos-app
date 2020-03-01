99ed16db175f086d64475e899e2cb9e28f50bf6b
Appy Sharma <appy70890@gmail.com>
2020-03-01 12:15:11 +0100

    startpage

---
+++ src/Pages/StartPage/StartPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+export default class StartPage extends Component {
+    render(){
+        return(
+<div className="startpage">
+    <div className="startpage-container">
+            <div className="startpage-img">
+
+            </div>
+            <div className="startpage-content">
+<h1>Order Here</h1>
+            </div>
+    </div>
+</div>
+        );
+    }
+}
